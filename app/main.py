4f63d150712babf149cd92377931d9a4300a6663
Mai-Linh Destiny Vu <mailinhv@uci.edu>
2026-07-30 18:12:22 -0700

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    pass
+    sys.stdout.write("$ ") #no new line, can use print("$ ", end="")
+
+    # Wait for user input
+    command = input()
+    print(f"$ {command}: command not found") #print for newline
 
 
 if __name__ == "__main__":

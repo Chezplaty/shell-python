7a4e3341e0968cba697b12a069109b4865bf467c
Mai-Linh Destiny Vu <mailinhv@uci.edu>
2026-07-30 20:50:07 -0700

    style: split user input into two parts

--- app/main.py
+++ app/main.py
@@ -7,25 +7,29 @@ def main():
         sys.stdout.write("$ ") #no new line, can use print("$ ", end="")
 
         # Wait for user input
-        command = input()
+        line = input()
+
+        parts = line.split(maxsplit=1)
+        cmd = parts[0]
+        arg = parts[1] if len(parts) > 1 else ""
 
         # Exit
-        if command == "exit":
+        if cmd == "exit":
             break
 
         # Echo (print)
-        if command.startswith("echo"):
-            print(command[5:])
+        if cmd.startswith("echo"):
+            print(arg)
 
         # Type - description of command type
-        elif command.startswith("type"):
-            if (command[5:]) in BUILTINS:
-                print(f"{command[5:]} is a shell builtin")
+        elif cmd.startswith("type"):
+            if (arg) in BUILTINS:
+                print(f"{arg} is a shell builtin")
             else:
-                print(f"{command[5:]}: not found")
+                print(f"{arg}: not found")
 
         else:
-            print(f"{command}: command not found") #print for newline
+            print(f"{arg}: command not found") #print for newline
 
 
 if __name__ == "__main__":

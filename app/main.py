16a5ab7e334594c14fff0fb8bda6c839e091721b
Mai-Linh Destiny Vu <mailinhv@uci.edu>
2026-07-30 21:25:48 -0700

    feat: Added onto type builtin. Able to look through PATH env variable for commands [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 import sys
+import os
+from pathlib import Path
 
 BUILTINS = {"echo", "exit", "type"}
+
+def find_command(command: str) -> Path | None:
+    system_path = os.getenv("PATH", "")
+
+    for directory in system_path.split(os.pathsep):
+        full_path = Path(directory)/command
+        if full_path.exists() and os.access(full_path, os.X_OK): #X_OK tests permissions
+            return full_path
+
+    return None
+
 
 def main():
     while True:
@@ -23,10 +36,14 @@ def main():
 
         # Type - description of command type
         elif cmd.startswith("type"):
-            if (arg) in BUILTINS:
+            if arg in BUILTINS:
                 print(f"{arg} is a shell builtin")
             else:
-                print(f"{arg}: not found")
+                path = find_command(arg)
+                if path:
+                    print(f"{arg} is {path}")
+                else:
+                    print(f"{arg}: not found")
 
         else:
             print(f"{arg}: command not found") #print for newline

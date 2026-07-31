86dd8720873ca559365984bfbfe9a2ec9dd234da
Mai-Linh Destiny Vu <mailinhv@uci.edu>
2026-07-30 18:22:37 -0700

    feat: added infinite loop [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ") #no new line, can use print("$ ", end="")
+    while True:
+        sys.stdout.write("$ ") #no new line, can use print("$ ", end="")
 
-    # Wait for user input
-    command = input()
-    print(f"{command}: command not found") #print for newline
+        # Wait for user input
+        command = input()
+        print(f"{command}: command not found") #print for newline
 
 
 if __name__ == "__main__":

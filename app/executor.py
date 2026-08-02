e7ce3d6103a8c41963ec2aad78a512ce6a597e89
Mai-Linh Destiny Vu <mailinhv@uci.edu>
2026-08-01 22:14:28 -0700

    feat: wired redirects to executor.py

--- app/executor.py
+++ app/executor.py
@@ -2,8 +2,9 @@ import subprocess
 
 from app.path_utils import get_executable
 from app.shell_builtins import BUILTINS
+from app.redirects import open_redirects, redirected_fds
 
-def handle_external_programs(cmd: str, args: list[str]) -> None:
+def handle_external_programs(cmd: str, args: list[str], files: dict[int, 'file']) -> None:
     """
     Executes an external command by locating its executable path through PATH.
     Runs the command with the provided arguments or prints an error if the command cannot be found.
@@ -13,7 +14,7 @@ def handle_external_programs(cmd: str, args: list[str]) -> None:
 
     program = get_executable(cmd)
     if program:
-        subprocess.run([cmd, *args]) #expand list
+        subprocess.run([cmd, *args], stdin=files.get(0), stdout=files.get(1), stderr=files.get(2))
     else:
         print(f"{cmd}: command not found")
 
@@ -23,12 +24,11 @@ def handle_command(instruction: Instruction) -> None:
     Prints an error message when the command is not supported by the shell.
     """
 
-    
-
-    handler = BUILTINS.get(instruction.cmd, "")
-
-    if handler:
-        handler(instruction)
-
-    else:
-        handle_external_programs(instruction.cmd, instruction.args)
+    with open_redirects(instruction) as files:
+        handler = BUILTINS.get(instruction.cmd, "")
+        if handler:
+            #change fds (rewire std outputs) if needed
+            with redirected_fds(files):
+                handler(instruction)
+        else:
+            handle_external_programs(instruction.cmd, instruction.args)

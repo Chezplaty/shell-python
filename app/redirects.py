162b9e1913ac4b288744fd1aef27d1c23a3ef57d
Mai-Linh Destiny Vu <mailinhv@uci.edu>
2026-08-01 22:06:28 -0700

    feat: added redirected_fds to change fds for builtins

--- app/redirects.py
+++ app/redirects.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from contextlib import contextmanager
 
 from app.lexer import TokenType
@@ -23,14 +25,15 @@ def resolve_redirect_targets(instruction: Instruction) -> dict[int, tuple[str, s
     return targets
 
 @contextmanager
-def open_redirects(instruction: Instruction):
+def open_redirects(instruction: Instruction) -> dict[int, 'file']:
     targets = resolve_redirect_targets(instruction)
     files = {}
-    
+
     try:
         # open all the files and yield them
         for fd, (path, mode) in targets.items():
             try:
+                #fd points wherever file object's fd points
                 files[fd] = open(path, mode)
             except OSError as e:
                 raise BuiltinError(instruction.cmd, f"{path}: {e.strerror}") from e
@@ -41,3 +44,28 @@ def open_redirects(instruction: Instruction):
         for file in files.values():
             file.close()
 
+@contextmanager
+def redirected_fds(files: dict[int, 'file']):
+
+    if not files:
+        yield
+        return
+
+    #empty everything
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    #get new un-used fd number that points to samce place original fd does
+    saved = {fd: os.dup(fd) for fd in files}
+
+    try:
+        for fd, file in files.items():
+            #make fd point to same place file's fd points to
+            os.dup2(file.fileno(), fd)
+        yield
+    finally:
+        sys.stdout.flush()
+        sys.stderr.flush()
+        for fd, saved_fd in saved.items():
+            os.dup2(saved_fd, fd) #repoint it back
+            os.close(saved_fd) #dont need bookmark anymore

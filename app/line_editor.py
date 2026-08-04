7c1aac4271a0c9b2c4da0bb5797402f05ede2704
Mai-Linh Destiny Vu <mailinhv@uci.edu>
2026-08-03 21:15:49 -0700

    feat: added tab autocompletion that cycles through candidates if available

--- app/line_editor.py
+++ app/line_editor.py
@@ -6,6 +6,18 @@ from app.shell_builtins import BUILTINS
 
 BUILTINS = sorted(BUILTINS)
 
+
+class CandidateCursor:
+
+    def __init__(self, candidates: list[str]):
+        self.candidates = candidates
+        self.index = 0
+
+    def next(self) -> str:
+        candidate = self.candidates[self.index]
+        self.index = (self.index + 1) % len(self.candidates) #wrap around
+        return candidate
+
 #TODO: replace with bisect later
 def find_insertion_point(prefix: str) -> int:
     """
@@ -50,29 +62,33 @@ def redraw(output: str) -> None:
     sys.stdout.write(f"$ {output}")
     sys.stdout.flush()
 
-def autocomplete(buffer: list[str]) -> list[str]:
-    """
-    Replaces the current buffer with the first matching completion.
-    Emits a terminal bell when no completion candidates are found.
-    """
+# def autocomplete(buffer: list[str]) -> list[str]:
+#     """
+#     Replaces the current buffer with the first matching completion.
+#     Emits a terminal bell when no completion candidates are found.
+#     """
 
-    candidates = get_candidates("".join(buffer))
+#     candidates = get_candidates("".join(buffer))
 
-    if candidates:
-        buffer.clear()
-        buffer.extend(candidates[0])
-        redraw(candidates[0])
-    else:
-        sys.stdout.write('\x07') #bell sound
-        sys.stdout.flush()
+#     #choose candidate
+#     cursor = CandidateCursor(candidates)
 
-    return buffer
+#     if candidates:
+#         buffer.clear()
+#         buffer.extend(candidates[0])
+#         redraw(candidates[0])
+#     else:
+#         sys.stdout.write('\x07') #bell sound
+#         sys.stdout.flush()
+
+#     return buffer
 
 def line_editor():
     #TODO: turn into context manager
     fd = sys.stdin.fileno()
     old_settings = tty.setcbreak(fd)
     buffer = []
+    tab_cursor = None
     try:
 
         while True:
@@ -82,17 +98,32 @@ def line_editor():
             if key == '\n':
                 return "".join(buffer)
 
-            #backspace
+            #tab character
+            if key == '\t':
+
+                #has not pressed tab yet, get new candidates
+                if tab_cursor is None:
+                    candidates = get_candidates("".join(buffer))
+                    if candidates:
+                        tab_cursor = CandidateCursor(candidates)
+                    else: #no candidates found
+                        sys.stdout.write('\x07') #bell sound
+                        sys.stdout.flush()
+                        continue
+
+                candidate = tab_cursor.next()
+                redraw(candidate)
+                buffer.clear()
+                buffer.extend(candidate)
+                continue
+
+            tab_cursor = None
+
             if key == '\x7f':
                 if buffer:
                     buffer.pop()
                     sys.stdout.write("\b \b")
                     sys.stdout.flush()
-                continue
-
-            #tab character
-            elif key == '\t':
-                buffer = autocomplete(buffer)
                 continue
 
             sys.stdout.write(key)

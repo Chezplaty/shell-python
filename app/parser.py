36c8e62481a867d31435ee280e89cd1b7ef5703f
Mai-Linh Destiny Vu <mailinhv@uci.edu>
2026-08-14 17:17:35 -0700

    feat: parser checks if the end of input has a '&' symbol, lets Instruction know to run program in background

--- app/parser.py
+++ app/parser.py
@@ -4,10 +4,11 @@ from app.errors import ParseError
 
 class Instruction:
 
-    def __init__(self, cmd: str, args: list[str], redirects: list[(Redirect)]):
+    def __init__(self, cmd: str, args: list[str], redirects: list[(Redirect)], run_bg: bool):
         self.cmd = cmd
         self.args = args
         self.redirects = redirects
+        self.run_bg = run_bg
 
 Redirect = namedtuple('Redirect', ['type', 'target'])
 
@@ -29,19 +30,21 @@ def parse_overwrite(tokens: list[Token], i: int) -> tuple[Redirect, int]:
     return Redirect(tokens[i].type, target.value), i + 1
 
 def parse(tokens: list[Token]) -> Instruction:
+    if not tokens:
+        return Instruction("", [], [], False)
 
-    cmd = ""
+    run_bg = tokens[-1].value == '&' #check if last token is &
+    end = len(tokens) - 1 if run_bg else len(tokens)
+
+    cmd = tokens[0].value
     args = []
     redirects = []
-    i = 0 
+    i = 1 
 
-    while i < len(tokens):
+    while i < end:
         token = tokens[i]
 
-        if i == 0:
-            cmd = token.value
-
-        elif token.type == TokenType.WORD:
+        if token.type == TokenType.WORD:
             args.append(token.value)
 
         elif token.type in {
@@ -54,7 +57,7 @@ def parse(tokens: list[Token]) -> Instruction:
             redirects.append(redirect)
 
         i += 1
-        
-    return Instruction(cmd, args, redirects)
+
+    return Instruction(cmd, args, redirects, run_bg)
             
 

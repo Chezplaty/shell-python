905495cac867a6aa7d0575aa2b4badd8afca9adb
Mai-Linh Destiny Vu <mailinhv@uci.edu>
2026-08-17 21:27:48 -0700

    feat: completer scripts temporarily block signals from coming in so subprocess's waitpid() can reap the signal before the handler in main

--- app/completer_runner.py
+++ app/completer_runner.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import signal
 from types import MappingProxyType
 
 
@@ -21,5 +22,11 @@ def run_completer_script(
     path = paths[command]
     os.chmod(path, os.stat(path).st_mode | 0o111) # make path executable for testing purposes
     #TODO: implement error handling when path cannot be run
-    output = subprocess.run([path, *args], env=env_copy, capture_output=True, text=True)
+
+    signal.pthread_sigmask(signal.SIG_BLOCK, {signal.SIGCHLD})
+    try:
+        output = subprocess.run([path, *args], env=env_copy, capture_output=True, text=True)
+    finally:
+        signal.pthread_sigmask(signal.SIG_UNBLOCK, {signal.SIGCHLD})
+        
     return output.stdout.splitlines()
